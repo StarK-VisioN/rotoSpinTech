0a7a103a1ac97e6cad48a483f516cff3e5f05269
Rushi-Parekar11 <rushikeshparekar11@gmail.com>
2025-08-29 17:26:26 +0530

    backed code added

---
+++ backend/models/UserModel.js
@@ -0,0 +1,21 @@
+import pool from "../config/db.js";
+
+export async function createUser(name, position, workingId, password) {
+  const query = `
+    INSERT INTO users (name, position, working_id, password)
+    VALUES ($1, $2, $3, $4)
+    RETURNING id, name, position, working_id, created_at;
+  `;
+
+  const values = [name, position, workingId, password];
+  const { rows } = await pool.query(query, values);
+  return rows[0];
+}
+
+export async function findUserByWorkingId(workingId) {
+  const { rows } = await pool.query(
+    "SELECT * FROM users WHERE working_id = $1",
+    [workingId]
+  );
+  return rows[0];
+}
